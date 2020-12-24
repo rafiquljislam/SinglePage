7176b8b0b61c044ac76748a1076edae14b57dc2b
MD.Rafiqul Islam <59832121+rafiquljislam@users.noreply.github.com>
2020-12-24 16:47:39 +0300

    Create addtocart.py

---
+++ addtocart.py
@@ -0,0 +1,53 @@
+def post(self,request):
+        product_id = request.data['id']
+        product_obj = Product.objects.get(id=product_id)
+        # print(product_obj,"product_obj")        
+        cart_cart = Cart.objects.filter(customer=request.user.profile).filter(complit=False).first()
+        cart_product_obj = CartProduct.objects.filter(product__id=product_id).first()
+        
+        try:
+            if cart_cart:
+                # print(cart_cart)
+                # print("OLD CART")
+                this_product_in_cart = cart_cart.cartproduct_set.filter(product=product_obj)
+                if this_product_in_cart.exists():
+                    # print("OLD CART PRODUCT--OLD CART")
+                    cartprod_uct = CartProduct.objects.filter(product=product_obj).filter(cart__complit=False).first()
+                    cartprod_uct.quantity +=1
+                    cartprod_uct.subtotal +=product_obj.selling_price
+                    cartprod_uct.save()
+                    cart_cart.total +=product_obj.selling_price
+                    cart_cart.save()
+                else:
+                    # print("NEW CART PRODUCT CREATED--OLD CART")
+                    cart_product_new=CartProduct.objects.create(
+                        cart = cart_cart,
+                        price  =product_obj.selling_price,
+                        quantity = 1,
+                        subtotal = product_obj.selling_price
+                    )
+                    cart_product_new.product.add(product_obj)
+                    cart_cart.total +=product_obj.selling_price
+                    cart_cart.save()
+            else:
+                # print(cart_cart)
+                # print("NEW CART CREATED")
+                Cart.objects.create(customer=request.user.profile,total=0,complit=False)
+                new_cart = Cart.objects.filter(customer=request.user.profile).filter(complit=False).first()
+                cart_product_new=CartProduct.objects.create(
+                        cart = new_cart,
+                        price  =product_obj.selling_price,
+                        quantity = 1,
+                        subtotal = product_obj.selling_price
+                    )
+                cart_product_new.product.add(product_obj)
+                # print("NEW CART PRODUCT CREATED")    
+                new_cart.total +=product_obj.selling_price
+                new_cart.save()
+
+            response_mesage = {'error':False,'message':"Product add to card successfully","productid":product_id}
+        
+        except:
+            response_mesage = {'error':True,'message':"Product Not add!Somthing is Wromg"}
+
+        return Response(response_mesage)
